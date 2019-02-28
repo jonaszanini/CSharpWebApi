81aca896252d7414b94e53cb78796f85b1382682
JONAS ZANINI <jzanini@br.ibm.com>
2019-02-28 20:40:22 -0300

    The project creation

---
+++ WebAPIModule1/Controllers/ProductsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPIModule1.Module;
+
+namespace WebAPIModule1.Controllers
+{
+    [Route("api/Products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        static List<Product> _products = new List<Product>()
+        {
+            new Product () {ProductID = 0, ProductName = "LapTop", ProductPrice = "100" },
+            new Product () {ProductID = 1, ProductName = "Mouse", ProductPrice = "20" }
+        };
+
+        public IActionResult Get()
+        {
+            return Ok(_products);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]Product product)
+        {
+            _products.Add(product);
+            
+            return StatusCode(StatusCodes.Status201Created);
+            
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Product product)
+        {
+            _products[id] = product;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _products.RemoveAt(id);
+        }
+    }
+}
---
+++ WebAPIModule1/Module/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIModule1.Module
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string ProductPrice { get; set; }
+    }
+}
